63750981afd3f8eeae6ad9920b3ecab7a2356c14
hoanglv2 <hoanglv2@hblab.vn>
2024-06-06 16:27:19 +0700

    third commit

--- src/views/Example/ChildComponent.js
+++ src/views/Example/ChildComponent.js
@@ -1,18 +1,68 @@
 import React from "react";
 class ChildComponent extends React.Component {
-state = {
-    firstName: '',
-    lastName: ''
-}
+    state = {
+        showJobs: false
+    }
+    handleShowHide = () => {
+        this.setState({
+            showJobs: !this.state.showJobs
+        })
+    }
+    handleOnClickDelete = (job) => {
+        console.log('handleOnClickDelete:', job)
+        this.props.deleteAJob(job)
+    }
     render() {
-        console.log('Check props: ', this.props)
-        let {name, age} = this.props
+        let { name, age, arrJobs } = this.props
+        let { showJobs } = this.state;
         return (
             <>
-               <div>ChildComponent {name} - {age}</div>
+                <div>
+                    {name}-{age}
+                </div>
+                {showJobs === false ?
+                    <div>
+                        <button onClick={() => this.handleShowHide()}>Show</button>
+                    </div>
+                    :
+                    <>
+                        <div className="job-list">
+                            {
+                                arrJobs.map((item, index) => {
+                                    return (
+                                        <div key={item.id}>
+                                            {item.title} - {item.salary}
+                                            <></> <span onClick={() =>this.handleOnClickDelete(item)}>X</span>
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
+                        <div>
+                            <button onClick={() => this.handleShowHide()}>Hide</button>
+                        </div>
+                    </>
+                }
             </>
         )
     }
 }
-
+// const ChildComponent = (props) => {
+//     let {arrJobs} = props;
+//     return (
+//         <>
+//             <div className="job-list">
+//                 {
+//                     arrJobs.map((item, index) => {
+//                         return (
+//                             <div key={item.id}>
+//                                 {item.title} - {item.salary}
+//                             </div>
+//                         )
+//                     })
+//                 }
+//             </div>
+//         </>
+//     )
+// }
 export default ChildComponent;
--- src/views/Example/MyComponent.js
+++ src/views/Example/MyComponent.js
@@ -1,50 +1,38 @@
 import React from "react";
 import ChildComponent from "./ChildComponent";
+import AddComponent from "./AddComponent";
 class MyComponent extends React.Component {
-state = {
-    firstName: '',
-    lastName: ''
-}
-handleChangeFirstName = (event) => {
-    this.setState({
-        firstName: event.target.value
-    })
-}
-handleChangeLastName = (event) => {
-    this.setState({
-        lastName: event.target.value
-    })
-}
-handleOnClick = (event) => {
-    event.preventDefault()
-    console.log('Check input: ', this.state)
-    
-}
+    state = {
+        arrJobs: [
+            { id: 'abcJob1', title: 'Developers', salary: '$500' },
+            { id: 'abcJob2', title: 'Testers', salary: '$400' },
+            { id: 'abcJob3', title: 'PM', salary: '$1000' }
+        ]
+    }
+    addNewJob = (job) => {
+        console.log('check job from parent: ', job)
+        this.setState({
+            arrJobs: [...this.state.arrJobs, job]
+        })
+    }
+    deleteAJob = (job) =>{
+        let currenJob = this.state.arrJobs
+        currenJob = currenJob.filter(item => item.id !== job.id)
+        this.setState({
+            arrJobs: currenJob
+        })
+    }
     render() {
         return (
             <>
-                <form action="/action_page.php">
-                    <label htmlFor="fname">First name:</label><br />
-                    <input type="text" 
-                    value= {this.state.firstName}
-                    onChange={(event) => this.handleChangeFirstName(event)}
-                    />
-                    <br />
-                    <label htmlFor="lname">Last name:</label><br />
-                    <input type="text" 
-                    value= {this.state.lastName} 
-                    onChange={(event) => this.handleChangeLastName(event)}
-                    />
-                    <br />
-                    <br />
-                    <input 
-                    type="submit"
-                    onClick={(event) => this.handleOnClick(event)}
-                    />
-                </form>
+                <AddComponent
+                    addNewJob = {this.addNewJob}
+                />
                 <ChildComponent
-                name = {'Hoàng'}
-                age = {'24'}
+                    name={'Hoàng'}
+                    age={'24'}
+                    arrJobs={this.state.arrJobs}
+                    deleteAJob = {this.deleteAJob}
                 />
             </>
         )
